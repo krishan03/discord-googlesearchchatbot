9085ca945413d8ecc516473e06297ae7800402cc
krishansharma01 <krishan.sharma01@nagarro.com>
2019-11-19 11:06:00 +0530

    decoded uri

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const googleIt = require('google-it');
 const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:P@ssw0rd@discordbot-gxec1.mongodb.net/test?retryWrites=true&w=majority";
 
-//mongoDB connection string on Atlas
-const uri = "mongodb+srv://<username>:<password>@discordbot-gxec1.mongodb.net/test?retryWrites=true&w=majority";
+
 
 
 client.on('ready', () => {
@@ -14,23 +14,21 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 
-    //replying to hi
     if (msg.content === 'hi') {
         msg.reply('Hey');
     }
-    //replying to !google search
     else if (msg.content.substring(0, 7) == '!google') {
         var query = msg.content.substring(7).trim();
         msg.reply('Please wait.. Google search is working..');
-        //calling Google Search API
-        googleIt({ 'query': query }).then(results => {
-            results.splice(0, 5).forEach(element => {
-                msg.reply(element.link);
+        //console.log(query);
+        googleIt({ 'query': query, 'limit': 5 }).then(results => {
+            results.forEach(element => {
+                msg.reply(decodeURIComponent(decodeURIComponent(element.link)));
             });
         }).catch(e => {
+            //console.log(e);
             msg.reply('Please try again after sometime');
         });
-        //inserting search result in Database in MongoDB atlas
         const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
         mongoClient.connect(err => {
             const collection = mongoClient.db("bot").collection("history");
@@ -49,7 +47,6 @@ client.on('message', msg => {
         });
 
     }
-    //hsowing recent search from persistent store i.e., MongoDB
     else if (msg.content.substring(0, 7) == '!recent') {
         var args = msg.content.substring(1).split(' ');
         var query = args[1];
@@ -57,6 +54,7 @@ client.on('message', msg => {
             const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
             mongoClient.connect(err => {
                 const collection = mongoClient.db("bot").collection("history");
+                // perform actions on the collection object
                 collection.findOne({ '_id': '5dd25adc7dfd7e37c0cf3b02' },
                     function (err, result) {
                         if (err) {
@@ -65,11 +63,11 @@ client.on('message', msg => {
                             if (result.history.length == 0) {
                                 msg.reply("no search history, please searh first");
                             }
-                            else if (result.history.length == 1 || result.history.length == 2) {
+                            else if (result.history.length == 1) {
                                 msg.reply(result.history);
                             }
                             else {
-                                msg.reply(result.history.splice(-3));
+                                msg.reply(result.history.splice(-2));
                             }
                         }
                     }
